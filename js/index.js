843f13f1a9af8c01e01a96718f3ffc8b3735ac76
Raju <rajutamang3456@gmail.com>
2025-03-20 16:55:25 +0545

    day-5

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// alert("are you sure?");
+// if (confirm("are you sure?")) {
+//   console.log("this is my first class");
+// }
+// let xyz = confirm("are you sure??");
+// console.log(xyz);
+// if (xyz) {
+//   console.log("friend request acepted");
+// } else {
+//   console.log("you have been blocked");
+// }
+
+// let yourPromt = prompt("entery your good name");
+// console.log(yourPromt);
