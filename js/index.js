58fcc10303e973925978b3b32005b338dfde1b33
Raju <rajutamang3456@gmail.com>
2025-03-31 17:06:43 +0545

    hello word

--- js/index.js
+++ js/index.js
@@ -18,3 +18,12 @@ const demo = document.getElementById("myPara");
 button.addEventListener("click", function () {
   demo.textContent = "Good afternoon all!!";
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
