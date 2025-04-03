c052c641109f65dc777cf87ea5c984eb805dda76
Raju <rajutamang3456@gmail.com>
2025-04-03 16:54:42 +0545

    operator and conditions

---
+++ js/conditions.js
@@ -0,0 +1,56 @@
+// let isRain = true; //camel Case
+// let isSnow = false;
+// let today = "monday";
+// let today1 = Date.now(); //how to get date  day (assignment)
+// if (today == "sunday") {
+//   console.log("Today is Monday");
+// } else if (today == "tuesday") {
+//   console.log("Today is Tuesday");
+// } else if (today == "monday") {
+//   console.log("Today is Monday");
+// } else {
+//   console.log("Today is sunday");
+// }
+
+// if (isSnow) {
+//   console.log("play in the snow");
+// } else {
+//   console.log("no need to take umbrella or rain coat");
+// }
+
+// aritmatic operator
+// let num1 = 10;
+// let num2 = 5;
+// let num3 = 2;
+// let num4 = num1 + num2 + num3; //addition
+// let num5 = num1 - num2 - num3; //subtraction
+// let num6 = num1 * num2 * num3; //multiplication
+// let num7 = num1 / num2 / num3; //division
+// let num8 = num1 % num2; //modulus
+// console.log(num4);
+
+//comparison operator
+// let num9 = 10;
+// let num10 = "ten";
+// let num11 = num9 == num10; //equal to
+// let num12 = num9 != num10; //not equal to
+// let num13 = num9 > num10; //greater than
+// let num14 = num9 < num10; //less than
+// let num15 = num9 >= num10; //greater than or equal to
+// let num16 = num9 <= num10; //less than or equal to
+// console.log(num11);
+
+let scorchinSun = true; //assigning value
+let isRainy = true;
+let cloudyDay = false;
+
+if (!scorchinSun || !isRainy) {
+  console.log("take umbrella");
+} else {
+  console.log("umbrella no need");
+}
+// Operands and operators
+// assignment
+// arithmetic
+// comparison < <= > >=  , == ,===
+// logical  &&  || !
